ce680f7e3ff5bb9c845a87f9c702f5ab4bfd4b89
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-05 00:34:40 +0800

    Update angka.js

--- posts/jawa/angka.js
+++ posts/jawa/angka.js
@@ -1,20 +1,12 @@
 const ANGKA_JAWA = {
-  0:'꧐',
-  1:'꧑',
-  2:'꧒',
-  3:'꧓',
-  4:'꧔',
-  5:'꧕',
-  6:'꧖',
-  7:'꧗',
-  8:'꧘',
-  9:'꧙'
+  0:'꧐',1:'꧑',2:'꧒',3:'꧓',4:'꧔',
+  5:'꧕',6:'꧖',7:'꧗',8:'꧘',9:'꧙'
 };
 
-const PADA_PANGKAT = '꧇'; // tanda angka Jawa
+const PADA_PANGKAT = '꧇';
 
-function latinNumberToJawa(numString){
+function latinNumberToJawa(num){
   return PADA_PANGKAT +
-    numString.split('').map(n => ANGKA_JAWA[n] || '').join('')
+    num.split('').map(n=>ANGKA_JAWA[n]).join('')
     + PADA_PANGKAT;
 }
