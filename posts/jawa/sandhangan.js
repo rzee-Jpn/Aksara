793288f0185754462e5de6829edd41215049403d
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-05 00:34:23 +0800

    Update sandhangan.js

--- posts/jawa/sandhangan.js
+++ posts/jawa/sandhangan.js
@@ -1,83 +1,33 @@
-/* =========================
-   AKSARA LEGENA (konsonan dasar)
-========================= */
 const AKSARA = {
   h:'ꦲ', n:'ꦤ', c:'ꦕ', r:'ꦫ', k:'ꦏ', d:'ꦢ', t:'ꦠ', s:'ꦱ',
   w:'ꦮ', l:'ꦭ', p:'ꦥ', j:'ꦗ', y:'ꦪ', m:'ꦩ', g:'ꦒ',
   b:'ꦧ', ng:'ꦔ', ny:'ꦚ', dh:'ꦝ', th:'ꦛ'
 };
 
-/* =========================
-   PASANGAN (index pasangan)
-   NOTE: representasi sederhana `'꧀'+aksara` jadi bentuk pasangan
-========================= */
-const PASANGAN = {
-  h:'꧀ꦲ', n:'꧀ꦤ', c:'꧀ꦕ', r:'꧀ꦫ', k:'꧀ꦏ', d:'꧀ꦢ', t:'꧀ꦠ',
-  s:'꧀ꦱ', w:'꧀ꦮ', l:'꧀ꦭ', p:'꧀ꦥ', j:'꧀ꦗ', y:'꧀ꦪ',
-  m:'꧀ꦩ', g:'꧀ꦒ', b:'꧀ꦧ',
-  ng:'꧀ꦔ', ny:'꧀ꦚ', dh:'꧀ꦝ', th:'꧀ꦛ'
-};
+const PASANGAN = Object.fromEntries(
+  Object.entries(AKSARA).map(([k,v]) => [k,'꧀'+v])
+);
 
-/* =========================
-   SANDHANGAN (vokal)
-========================= */
 const SANDHANGAN = {
-  i:'ꦶ',   // wulu
-  u:'ꦸ',   // suku
-  e:'ꦺ',   // taling
-  o:'ꦺꦴ', // taling tarung
-  ê:'ꦼ'    // pepet
+  i:'ꦶ', u:'ꦸ', e:'ꦺ', o:'ꦺꦴ', ê:'ꦼ'
 };
 
 const PANGKON = '꧀';
 
-/* =========================
-   PANYIGEG WANDA (akhiran khusus)
-========================= */
 const PANYIGEG = {
-  ng:'ꦁ', // cecak
-  r :'ꦂ', // layar
-  h :'ꦃ'  // wignyan
+  ng:'ꦁ', r:'ꦂ', h:'ꦃ'
 };
 
-/* =========================
-   AKSARA SWARA (huruf mandiri / vokal di awal)
-========================= */
 const SWARA = {
-  a: 'ꦄ',
-  i: 'ꦆ',
-  u: 'ꦈ',
-  e: 'ꦌ',
-  o: 'ꦎ'
+  a:'ꦄ', i:'ꦆ', u:'ꦈ', e:'ꦌ', o:'ꦎ'
 };
 
-/* =========================
-   AKSARA MURDA (opsional)
-   Hanya beberapa huruf (sesuai praktik)
-========================= */
 const MURDA = {
-  n: 'ꦟ',
-  k: 'ꦑ',
-  t: 'ꦡ',
-  s: 'ꦯ',
-  p: 'ꦦ',
-  b: 'ꦨ',
-  g: 'ꦓ',
-  c: 'ꦖ',
-  j: 'ꦙ',
-  d: 'ꦣ'
+  n:'ꦟ', k:'ꦑ', t:'ꦡ', s:'ꦯ', p:'ꦦ',
+  b:'ꦨ', g:'ꦓ', c:'ꦖ', j:'ꦙ', d:'ꦣ'
 };
 
-/* =========================
-   AKSARA SERAPAN (digraf/karakter non-standar untuk serapan)
-   Periksa paling awal saat transliterasi
-========================= */
 const SERAPAN = {
-  f: 'ꦥ꦳',   // fa -> pa + cakra telu
-  v: 'ꦮ꦳',   // va
-  z: 'ꦗ꦳',   // za
-  sy: 'ꦯ',    // sya (menggunakan aksara saw)
-  kh: 'ꦑ꦳',   // kha
-  gh: 'ꦒ꦳',   // gha
-  q: 'ꦐ'      // qa
+  f:'ꦥ꦳', v:'ꦮ꦳', z:'ꦗ꦳', sy:'ꦯ',
+  kh:'ꦑ꦳', gh:'ꦒ꦳', q:'ꦐ'
 };
