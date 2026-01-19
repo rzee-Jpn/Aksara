491fbf8d160bb1031de6ba34d04ed81956f4d8d6
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-19 17:13:18 +0800

    Create n4.js

---
+++ posts/kanji/data/kanji/n4.js
@@ -0,0 +1,799 @@
+const N4 = [
+  {
+    c: "会",
+    on: ["カイ"],
+    kun: ["あ"],
+    a: "bertemu",
+    jlpt: "N4"
+  },
+  {
+    c: "社",
+    on: ["シャ"],
+    kun: ["やしろ"],
+    a: "perusahaan",
+    jlpt: "N4"
+  },
+  {
+    c: "店",
+    on: ["テン"],
+    kun: ["みせ"],
+    a: "toko",
+    jlpt: "N4"
+  },
+  {
+    c: "場",
+    on: ["ジョウ"],
+    kun: ["ば"],
+    a: "tempat",
+    jlpt: "N4"
+  },
+  {
+    c: "道",
+    on: ["ドウ"],
+    kun: ["みち"],
+    a: "jalan",
+    jlpt: "N4"
+  },
+  {
+    c: "国",
+    on: ["コク"],
+    kun: ["くに"],
+    a: "negara",
+    jlpt: "N4"
+  },
+  {
+    c: "外",
+    on: ["ガイ"],
+    kun: ["そと"],
+    a: "luar",
+    jlpt: "N4"
+  },
+  {
+    c: "市",
+    on: ["シ"],
+    kun: ["いち"],
+    a: "kota",
+    jlpt: "N4"
+  },
+  {
+    c: "町",
+    on: ["チョウ"],
+    kun: ["まち"],
+    a: "kota kecil",
+    jlpt: "N4"
+  },
+  {
+    c: "村",
+    on: ["ソン"],
+    kun: ["むら"],
+    a: "desa",
+    jlpt: "N4"
+  },
+  {
+    c: "地",
+    on: ["チ", "ジ"],
+    kun: [],
+    a: "tanah / area",
+    jlpt: "N4"
+  },
+  {
+    c: "図",
+    on: ["ズ"],
+    kun: ["はか"],
+    a: "peta",
+    jlpt: "N4"
+  },
+  {
+    c: "室",
+    on: ["シツ"],
+    kun: ["むろ"],
+    a: "ruangan",
+    jlpt: "N4"
+  },
+  {
+    c: "院",
+    on: ["イン"],
+    kun: [],
+    a: "institusi / rumah sakit",
+    jlpt: "N4"
+  },
+  {
+    c: "館",
+    on: ["カン"],
+    kun: ["やかた"],
+    a: "gedung",
+    jlpt: "N4"
+  },
+  {
+    c: "所",
+    on: ["ショ"],
+    kun: ["ところ"],
+    a: "tempat",
+    jlpt: "N4"
+  },
+  {
+    c: "島",
+    on: ["トウ"],
+    kun: ["しま"],
+    a: "pulau",
+    jlpt: "N4"
+  },
+  {
+    c: "海",
+    on: ["カイ"],
+    kun: ["うみ"],
+    a: "laut",
+    jlpt: "N4"
+  },
+  {
+    c: "空",
+    on: ["クウ"],
+    kun: ["そら", "あ"],
+    a: "langit / kosong",
+    jlpt: "N4"
+  },
+  {
+    c: "港",
+    on: ["コウ"],
+    kun: ["みなと"],
+    a: "pelabuhan",
+    jlpt: "N4"
+  },
+  {
+    c: "家",
+    on: ["カ"],
+    kun: ["いえ", "や"],
+    a: "rumah",
+    jlpt: "N4"
+  },
+  {
+    c: "族",
+    on: ["ゾク"],
+    kun: [],
+    a: "keluarga (kelompok)",
+    jlpt: "N4"
+  },
+  {
+    c: "親",
+    on: ["シン"],
+    kun: ["おや"],
+    a: "orang tua",
+    jlpt: "N4"
+  },
+  {
+    c: "兄",
+    on: ["ケイ"],
+    kun: ["あに"],
+    a: "kakak laki-laki",
+    jlpt: "N4"
+  },
+  {
+    c: "姉",
+    on: ["シ"],
+    kun: ["あね"],
+    a: "kakak perempuan",
+    jlpt: "N4"
+  },
+  {
+    c: "弟",
+    on: ["テイ"],
+    kun: ["おとうと"],
+    a: "adik laki-laki",
+    jlpt: "N4"
+  },
+  {
+    c: "妹",
+    on: ["マイ"],
+    kun: ["いもうと"],
+    a: "adik perempuan",
+    jlpt: "N4"
+  },
+  {
+    c: "夫",
+    on: ["フ"],
+    kun: ["おっと"],
+    a: "suami",
+    jlpt: "N4"
+  },
+  {
+    c: "妻",
+    on: ["サイ"],
+    kun: ["つま"],
+    a: "istri",
+    jlpt: "N4"
+  },
+  {
+    c: "主",
+    on: ["シュ"],
+    kun: ["ぬし", "おも"],
+    a: "utama / pemilik",
+    jlpt: "N4"
+  },
+
+  // Kehidupan & kondisi
+  {
+    c: "生",
+    on: ["セイ", "ショウ"],
+    kun: ["い"],
+    a: "hidup",
+    jlpt: "N4"
+  },
+  {
+    c: "活",
+    on: ["カツ"],
+    kun: [],
+    a: "kehidupan / aktivitas",
+    jlpt: "N4"
+  },
+  {
+    c: "住",
+    on: ["ジュウ"],
+    kun: ["す"],
+    a: "tinggal",
+    jlpt: "N4"
+  },
+  {
+    c: "使",
+    on: ["シ"],
+    kun: ["つか"],
+    a: "menggunakan",
+    jlpt: "N4"
+  },
+  {
+    c: "作",
+    on: ["サク"],
+    kun: ["つく"],
+    a: "membuat",
+    jlpt: "N4"
+  },
+  {
+    c: "持",
+    on: ["ジ"],
+    kun: ["も"],
+    a: "membawa / memiliki",
+    jlpt: "N4"
+  },
+  {
+    c: "送",
+    on: ["ソウ"],
+    kun: ["おく"],
+    a: "mengirim",
+    jlpt: "N4"
+  },
+  {
+    c: "受",
+    on: ["ジュ"],
+    kun: ["う"],
+    a: "menerima",
+    jlpt: "N4"
+  },
+  {
+    c: "取",
+    on: ["シュ"],
+    kun: ["と"],
+    a: "mengambil",
+    jlpt: "N4"
+  },
+  // N4 – Sesi 3
+  
+  // Kesehatan & kondisi tubuh
+  {
+    c: "病",
+    on: ["ビョウ"],
+    kun: ["や"],
+    a: "sakit / penyakit",
+    jlpt: "N4"
+  },
+  {
+    c: "医",
+    on: ["イ"],
+    kun: [],
+    a: "dokter / medis",
+    jlpt: "N4"
+  },
+  {
+    c: "者",
+    on: ["シャ"],
+    kun: ["もの"],
+    a: "orang",
+    jlpt: "N4"
+  },
+  {
+    c: "死",
+    on: ["シ"],
+    kun: ["し"],
+    a: "mati",
+    jlpt: "N4"
+  },
+  {
+    c: "痛",
+    on: ["ツウ"],
+    kun: ["いた"],
+    a: "sakit / nyeri",
+    jlpt: "N4"
+  },
+  {
+    c: "疲",
+    on: ["ヒ"],
+    kun: ["つか"],
+    a: "lelah",
+    jlpt: "N4"
+  },
+  {
+    c: "弱",
+    on: ["ジャク"],
+    kun: ["よわ"],
+    a: "lemah",
+    jlpt: "N4"
+  },
+  {
+    c: "強",
+    on: ["キョウ"],
+    kun: ["つよ"],
+    a: "kuat",
+    jlpt: "N4"
+  },
+  {
+    c: "元",
+    on: ["ゲン"],
+    kun: ["もと"],
+    a: "asal / sehat",
+    jlpt: "N4"
+  },
+  {
+    c: "気",
+    on: ["キ"],
+    kun: [],
+    a: "energi / perasaan",
+    jlpt: "N4"
+  },
+
+  // Pikiran & perasaan
+  {
+    c: "意",
+    on: ["イ"],
+    kun: [],
+    a: "niat / maksud",
+    jlpt: "N4"
+  },
+  {
+    c: "思",
+    on: ["シ"],
+    kun: ["おも"],
+    a: "berpikir / merasa",
+    jlpt: "N4"
+  },
+  {
+    c: "知",
+    on: ["チ"],
+    kun: ["し"],
+    a: "mengetahui",
+    jlpt: "N4"
+  },
+  {
+    c: "考",
+    on: ["コウ"],
+    kun: ["かんが"],
+    a: "memikirkan",
+    jlpt: "N4"
+  },
+  {
+    c: "感",
+    on: ["カン"],
+    kun: [],
+    a: "merasakan / perasaan",
+    jlpt: "N4"
+  },
+  {
+    c: "心",
+    on: ["シン"],
+    kun: ["こころ"],
+    a: "hati / pikiran",
+    jlpt: "N4"
+  },
+  {
+    c: "配",
+    on: ["ハイ"],
+    kun: [],
+    a: "khawatir",
+    jlpt: "N4"
+  },
+  {
+    c: "決",
+    on: ["ケツ"],
+    kun: ["き"],
+    a: "memutuskan",
+    jlpt: "N4"
+  },
+  {
+    c: "選",
+    on: ["セン"],
+    kun: ["えら"],
+    a: "memilih",
+    jlpt: "N4"
+  },
+  {
+    c: "望",
+    on: ["ボウ"],
+    kun: ["のぞ"],
+    a: "harapan / berharap",
+    jlpt: "N4"
+  },
+
+  // Aktivitas & kerja
+  {
+    c: "働",
+    on: ["ドウ"],
+    kun: ["はたら"],
+    a: "bekerja",
+    jlpt: "N4"
+  },
+  {
+    c: "始",
+    on: ["シ"],
+    kun: ["はじ"],
+    a: "mulai",
+    jlpt: "N4"
+  },
+  {
+    c: "終",
+    on: ["シュウ"],
+    kun: ["お"],
+    a: "selesai",
+    jlpt: "N4"
+  },
+  {
+    c: "続",
+    on: ["ゾク"],
+    kun: ["つづ"],
+    a: "lanjut",
+    jlpt: "N4"
+  },
+  {
+    c: "変",
+    on: ["ヘン"],
+    kun: ["か"],
+    a: "berubah",
+    jlpt: "N4"
+  },
+  {
+    c: "進",
+    on: ["シン"],
+    kun: ["すす"],
+    a: "maju",
+    jlpt: "N4"
+  },
+  {
+    c: "止",
+    on: ["シ"],
+    kun: ["と"],
+    a: "berhenti",
+    jlpt: "N4"
+  },
+  {
+    c: "動",
+    on: ["ドウ"],
+    kun: ["うご"],
+    a: "bergerak",
+    jlpt: "N4"
+  },
+  {
+    c: "通",
+    on: ["ツウ"],
+    kun: ["とお", "かよ"],
+    a: "melewati",
+    jlpt: "N4"
+  },
+  {
+    c: "集",
+    on: ["シュウ"],
+    kun: ["あつ"],
+    a: "mengumpulkan",
+    jlpt: "N4"
+  },
+  // N4 – Sesi 4 (FINAL)
+
+  // Pendidikan & ujian
+  {
+    c: "教",
+    on: ["キョウ"],
+    kun: ["おし"],
+    a: "mengajar",
+    jlpt: "N4"
+  },
+  {
+    c: "習",
+    on: ["シュウ"],
+    kun: ["なら"],
+    a: "belajar / latihan",
+    jlpt: "N4"
+  },
+  {
+    c: "業",
+    on: ["ギョウ"],
+    kun: ["わざ"],
+    a: "pekerjaan / industri",
+    jlpt: "N4"
+  },
+  {
+    c: "試",
+    on: ["シ"],
+    kun: ["こころ"],
+    a: "ujian / mencoba",
+    jlpt: "N4"
+  },
+  {
+    c: "験",
+    on: ["ケン"],
+    kun: [],
+    a: "pengalaman",
+    jlpt: "N4"
+  },
+  {
+    c: "質",
+    on: ["シツ"],
+    kun: [],
+    a: "kualitas",
+    jlpt: "N4"
+  },
+  {
+    c: "問",
+    on: ["モン"],
+    kun: ["と"],
+    a: "bertanya",
+    jlpt: "N4"
+  },
+  {
+    c: "答",
+    on: ["トウ"],
+    kun: ["こた"],
+    a: "menjawab",
+    jlpt: "N4"
+  },
+
+  // Sifat & deskripsi
+  {
+    c: "多",
+    on: ["タ"],
+    kun: ["おお"],
+    a: "banyak",
+    jlpt: "N4"
+  },
+  {
+    c: "少",
+    on: ["ショウ"],
+    kun: ["すく"],
+    a: "sedikit",
+    jlpt: "N4"
+  },
+  {
+    c: "早",
+    on: ["ソウ"],
+    kun: ["はや"],
+    a: "cepat / pagi",
+    jlpt: "N4"
+  },
+  {
+    c: "遅",
+    on: ["チ"],
+    kun: ["おそ"],
+    a: "lambat",
+    jlpt: "N4"
+  },
+  {
+    c: "新",
+    on: ["シン"],
+    kun: ["あたら"],
+    a: "baru",
+    jlpt: "N4"
+  },
+  {
+    c: "古",
+    on: ["コ"],
+    kun: ["ふる"],
+    a: "lama",
+    jlpt: "N4"
+  },
+  {
+    c: "長",
+    on: ["チョウ"],
+    kun: ["なが"],
+    a: "panjang",
+    jlpt: "N4"
+  },
+  {
+    c: "短",
+    on: ["タン"],
+    kun: ["みじか"],
+    a: "pendek",
+    jlpt: "N4"
+  },
+  {
+    c: "重",
+    on: ["ジュウ"],
+    kun: ["おも"],
+    a: "berat",
+    jlpt: "N4"
+  },
+  {
+    c: "軽",
+    on: ["ケイ"],
+    kun: ["かる"],
+    a: "ringan",
+    jlpt: "N4"
+  },
+  {
+    c: "良",
+    on: ["リョウ"],
+    kun: ["よ"],
+    a: "baik",
+    jlpt: "N4"
+  },
+  {
+    c: "悪",
+    on: ["アク"],
+    kun: ["わる"],
+    a: "buruk",
+    jlpt: "N4"
+  },
+  {
+    c: "明",
+    on: ["メイ"],
+    kun: ["あか"],
+    a: "terang / jelas",
+    jlpt: "N4"
+  },
+  {
+    c: "暗",
+    on: ["アン"],
+    kun: ["くら"],
+    a: "gelap",
+    jlpt: "N4"
+  },
+
+  // Alam & cuaca
+  {
+    c: "雨",
+    on: ["ウ"],
+    kun: ["あめ"],
+    a: "hujan",
+    jlpt: "N4"
+  },
+  {
+    c: "雪",
+    on: ["セツ"],
+    kun: ["ゆき"],
+    a: "salju",
+    jlpt: "N4"
+  },
+  {
+    c: "風",
+    on: ["フウ"],
+    kun: ["かぜ"],
+    a: "angin",
+    jlpt: "N4"
+  },
+  {
+    c: "雲",
+    on: ["ウン"],
+    kun: ["くも"],
+    a: "awan",
+    jlpt: "N4"
+  },
+  {
+    c: "温",
+    on: ["オン"],
+    kun: ["あたた"],
+    a: "hangat",
+    jlpt: "N4"
+  },
+  {
+    c: "冷",
+    on: ["レイ"],
+    kun: ["つめ", "ひ"],
+    a: "dingin",
+    jlpt: "N4"
+  },
+
+  // Makanan & hewan
+  {
+    c: "魚",
+    on: ["ギョ"],
+    kun: ["さかな"],
+    a: "ikan",
+    jlpt: "N4"
+  },
+  {
+    c: "肉",
+    on: ["ニク"],
+    kun: [],
+    a: "daging",
+    jlpt: "N4"
+  },
+  {
+    c: "鳥",
+    on: ["チョウ"],
+    kun: ["とり"],
+    a: "burung",
+    jlpt: "N4"
+  },
+  {
+    c: "牛",
+    on: ["ギュウ"],
+    kun: ["うし"],
+    a: "sapi",
+    jlpt: "N4"
+  },
+  {
+    c: "米",
+    on: ["ベイ"],
+    kun: ["こめ"],
+    a: "beras",
+    jlpt: "N4"
+  },
+  {
+    c: "茶",
+    on: ["チャ"],
+    kun: [],
+    a: "teh",
+    jlpt: "N4"
+  },
+  {
+    c: "酒",
+    on: ["シュ"],
+    kun: ["さけ"],
+    a: "alkohol",
+    jlpt: "N4"
+  },
+
+  // Transportasi & alat
+  {
+    c: "車",
+    on: ["シャ"],
+    kun: ["くるま"],
+    a: "mobil",
+    jlpt: "N4"
+  },
+  {
+    c: "電",
+    on: ["デン"],
+    kun: [],
+    a: "listrik",
+    jlpt: "N4"
+  },
+  {
+    c: "駅",
+    on: ["エキ"],
+    kun: [],
+    a: "stasiun",
+    jlpt: "N4"
+  },
+  {
+    c: "船",
+    on: ["セン"],
+    kun: ["ふね"],
+    a: "kapal",
+    jlpt: "N4"
+  },
+  {
+    c: "橋",
+    on: ["キョウ"],
+    kun: ["はし"],
+    a: "jembatan",
+    jlpt: "N4"
+  },
+  {
+    c: "門",
+    on: ["モン"],
+    kun: ["かど"],
+    a: "gerbang",
+    jlpt: "N4"
+  },
+  {
+    c: "具",
+    on: ["グ"],
+    kun: [],
+    a: "alat",
+    jlpt: "N4"
+  }
+];
