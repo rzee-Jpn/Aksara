cdae423df33ce561c868e3824ee6c7eed6c51401
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-21 14:43:32 +0800

    Create hangeul_vowel.js

---
+++ posts/hangul/js/content/hangeul/hangeul_vowel.js
@@ -0,0 +1,8 @@
+export const VOWEL = [
+  { id:"a", category:"vowel", hangul:"ㅏ", tts:"아", options:["ㅏ","ㅓ","ㅗ"], answer:"ㅏ", weight:1, errorCount:0 },
+  { id:"eo", category:"vowel", hangul:"ㅓ", tts:"어", options:["ㅓ","ㅏ","ㅜ"], answer:"ㅓ", weight:1, errorCount:0 },
+  { id:"o", category:"vowel", hangul:"ㅗ", tts:"오", options:["ㅗ","ㅜ","ㅓ"], answer:"ㅗ", weight:1, errorCount:0 },
+  { id:"u", category:"vowel", hangul:"ㅜ", tts:"우", options:["ㅜ","ㅗ","ㅡ"], answer:"ㅜ", weight:1, errorCount:0 },
+  { id:"eu", category:"vowel", hangul:"ㅡ", tts:"으", options:["ㅡ","ㅜ","ㅣ"], answer:"ㅡ", weight:1, errorCount:0 },
+  { id:"i", category:"vowel", hangul:"ㅣ", tts:"이", options:["ㅣ","ㅡ","ㅓ"], answer:"ㅣ", weight:1, errorCount:0 }
+];
