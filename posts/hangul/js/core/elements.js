bec272985d7825ebb950ddcdf8fb2edf59bed09e
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-21 14:33:17 +0800

    Create core/elements.js

---
+++ posts/hangul/js/core/elements.js
@@ -0,0 +1,19 @@
+export const btnStart = document.getElementById("btnStart");
+export const btnSurvival = document.getElementById("btnSurvival");
+export const btnRevive = document.getElementById("btnRevive");
+export const btnRetry = document.getElementById("btnRetry");
+export const btnGoWorld = document.getElementById("btnGoWorld");
+export const btnPlayAudio = document.getElementById("btnPlayAudio");
+export const btnBackToWorld = document.getElementById("btnBackToWorld");
+
+export const btnHangeulBasic = document.getElementById("btnHangeulBasic");
+export const btnHangeulVowel = document.getElementById("btnHangeulVowel");
+export const btnHangeulInitial = document.getElementById("btnHangeulInitial");
+export const btnAssimilation = document.getElementById("btnAssimilation");
+export const btnBatchimL1 = document.getElementById("btnBatchimL1");
+export const btnBatchimL2 = document.getElementById("btnBatchimL2");
+export const btnBatchimL3 = document.getElementById("btnBatchimL3");
+export const btnBatchimL4 = document.getElementById("btnBatchimL4");
+
+export const btnFakeSubscribe = document.getElementById("btnFakeSubscribe");
+export const btnCloseSubscribe = document.getElementById("btnCloseSubscribe");
