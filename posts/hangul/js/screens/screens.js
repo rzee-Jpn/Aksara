4f84e0226bdf5c8fc9c4d20ffe1134f8db99a635
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-21 14:28:51 +0800

    Create screens/screens.js

---
+++ posts/hangul/js/screens/screens.js
@@ -0,0 +1,7 @@
+export function showScreen(id) {
+  document
+    .querySelectorAll(".screen")
+    .forEach(s => s.classList.remove("active"));
+
+  document.getElementById(id)?.classList.add("active");
+}
