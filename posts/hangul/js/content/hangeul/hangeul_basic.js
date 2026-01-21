7b75b8863b5ebdf0412e41d581b5ebc74d0ec6a5
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-01-21 14:42:13 +0800

    Create hangeul_basic.js

---
+++ posts/hangul/js/content/hangeul/hangeul_basic.js
@@ -0,0 +1,9 @@
+export const CONSONANT = [
+  { id:"g_f", category:"consonant", hangul:"ㄱ", tts:"악", options:["ㄱ","ㄴ","ㄷ"], answer:"ㄱ", weight:1, errorCount:0 },
+  { id:"n_f", category:"consonant", hangul:"ㄴ", tts:"안", options:["ㄴ","ㅁ","ㅇ"], answer:"ㄴ", weight:1, errorCount:0 },
+  { id:"d_f", category:"consonant", hangul:"ㄷ", tts:"닫", options:["ㄷ","ㅅ","ㅈ"], answer:"ㄷ", weight:1, errorCount:0 },
+  { id:"l_f", category:"consonant", hangul:"ㄹ", tts:"알", options:["ㄹ","ㄴ","ㅁ"], answer:"ㄹ", weight:1, errorCount:0 },
+  { id:"m_f", category:"consonant", hangul:"ㅁ", tts:"암", options:["ㅁ","ㄴ","ㅇ"], answer:"ㅁ", weight:1, errorCount:0 },
+  { id:"b_f", category:"consonant", hangul:"ㅂ", tts:"압", options:["ㅂ","ㅍ","ㄱ"], answer:"ㅂ", weight:1, errorCount:0 },
+  { id:"ng_f", category:"consonant", hangul:"ㅇ", tts:"앙", options:["ㅇ","ㄴ","ㅁ"], answer:"ㅇ", weight:1, errorCount:0 }
+];
